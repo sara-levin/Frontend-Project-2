06da51887229bd6197686f76341855527f6f7607
sara-levin <75952006+sara-levin@users.noreply.github.com>
2021-10-07 16:13:43 +0200

    Project 2 - JavaScript - version 2

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ const apiKey = '23565747-e2874a299c02424f88c571095';
 }
 
 var count = 1; //Keeps tack of page number
+var lastPageCount = null; //Disables the forward button 
 var islastpage = false //Makes user unable to go further than the last page
 const form = document.getElementById("searchform");
+form.backwards.disabled = true;
+form.forwards.disabled = true;
+var query = null;
+var selectedcolor = null;
 
 //#region Clearbox function
 function clearBox(elementID) {
@@ -17,35 +22,10 @@ function clearBox(elementID) {
 }
 //#endregion
 
-//#region When clicking "Submit"
-form.onsubmit = async event => {
-    count = 1;
-    event.preventDefault(); 
-    const query = form.elements.query.value
-    const selectedcolor = form.elements.colors.value
-    const params = new URLSearchParams ({
-        q: query,
-        image_type: "photo", //We chose to restirict the results to photos, simply because it was more aesthetically pleasing. Can of course be reversed. 
-        colors: selectedcolor,
-        per_page: 10,
-        page: count
-
-    })
-        
-    const response = await fetch ('https://pixabay.com/api/?key='+apiKey+'&'+params.toString());
-    const json = await response.json();
-
-    clearBox('bilder');
-
-    if (json.hits == 0){
-        alert("This search gave no hits, please try something else")
-    } 
-    else if (json <= 10){
-        islastpage = true
-    }
-    else{
-    islastpage = false
-    json.hits.forEach(element => 
+//#region imageLoop function
+function imageLoop(variable){
+    lastPageCount = 0;
+    variable.hits.forEach(element => 
         { 
             const newimg = document.createElement("figure");
             const img = document.createElement("img");
@@ -65,21 +45,58 @@ form.onsubmit = async event => {
             photocaption.appendChild(photographer);
             newimg.appendChild(photocaption)
             document.getElementById('bilder').appendChild(newimg);
+            lastPageCount++;
         });
+    if (lastPageCount<10){
+        form.forwards.disabled = true;}
+}
+//#endregion
+
+//#region When clicking "Submit"
+form.onsubmit = async event => {
+    count = 1;
+    event.preventDefault(); 
+    query = form.elements.query.value
+    selectedcolor = form.elements.colors.value
+    const params = new URLSearchParams ({
+        q: query,
+        image_type: "photo", //We chose to restirict the results to photos, simply because it was more aesthetically pleasing. Can of course be reversed. 
+        colors: selectedcolor,
+        per_page: 10,
+        page: count
+
+    })
+        
+    const response = await fetch ('https://pixabay.com/api/?key='+apiKey+'&'+params.toString());
+    const json = await response.json();
+
+    clearBox('bilder');
+
+    
+
+    if (json.hits == 0){
+        alert("This search gave no hits, please try something else")
+        form.forwards.disabled = false;
+    } 
+    else if (json.hits <= 10){
+        islastpage = true
+        
+        const forwards = document.getElementById('forwards')
+        forwards.disabled = true;
+    }
+    else{
+    islastpage = false
+    form.forwards.disabled = false;
+    imageLoop(json);
+
     }
 }
 //#endregion
 
 //#region When clicking "Backwards"
 form.backwards.onclick = async event =>{
-    if (count == 1){
-        alert("You are on the first page");
-    }
-    else {
-        count--;
-        event.preventDefault(); 
-    const query = form.elements.query.value
-    const selectedcolor = form.elements.colors.value
+    count--;
+    event.preventDefault(); 
     const params = new URLSearchParams ({
         q: query,
         image_type: "photo",
@@ -95,86 +112,35 @@ form.backwards.onclick = async event =>{
     clearBox('bilder');
     islastpage = false;
 
-    json.hits.forEach(element => 
-        { 
-            const newimg = document.createElement("figure");
-            const img = document.createElement("img");
-            img.src = element.webformatURL.replace("_640.jpg", "_180.jpg");
-            newimg.appendChild(img); 
+    imageLoop(json);
 
-            const tagcaption = document.createElement("figcaption");    
-            const tags = document.createTextNode(element.tags)
-            tagcaption.appendChild(tags);
-            newimg.appendChild(tagcaption);
-            document.getElementById('bilder').appendChild(newimg);
-
-            const photocaption = document.createElement("figcaption");
-            const photographer = document.createTextNode(element.user);
-            const photoby = document.createTextNode("Photo by: ");
-            photocaption.appendChild(photoby);
-            photocaption.appendChild(photographer);
-            newimg.appendChild(photocaption)
-            document.getElementById('bilder').appendChild(newimg);
-        });
+    form.forwards.disabled = false;
+    
+    if (count == 1){
+        backwards.disabled = true;
     }
     
 }
 //#endregion
 
 //#region When clicking "Forwards"
-form.forwards.onclick = async event =>{
-    if (islastpage == true){
-        alert("You are on the last page");
-    }
-    else {
+form.forwards.onclick = async event =>{ 
         count++;
         event.preventDefault(); 
-    const query = form.elements.query.value
-    const selectedcolor = form.elements.colors.value
-    const params = new URLSearchParams ({
-        q: query,
-        image_type: "photo",
-        colors: selectedcolor,
-        per_page: 10,
-        page: count
-
-    })
-        
+        const params = new URLSearchParams ({
+            q: query,
+            image_type: "photo",
+            colors: selectedcolor,
+            per_page: 10,
+            page: count
+        })
+    form.backwards.disabled = false;
     const response = await fetch ('https://pixabay.com/api/?key='+apiKey+'&'+params.toString());
     const json = await response.json();
 
-    if(json.hits <= 10){
-        islastpage = true
-        alert("This is the last page")
-        count--
-    }
-    else{
-        islastpage=false
-
-        clearBox('bilder');
+    clearBox('bilder');
         
-        json.hits.forEach(element => 
-            { 
-                const newimg = document.createElement("figure");
-                const img = document.createElement("img");
-                img.src = element.webformatURL.replace("_640.jpg", "_180.jpg");
-                newimg.appendChild(img); 
-    
-                const tagcaption = document.createElement("figcaption");    
-                const tags = document.createTextNode(element.tags)
-                tagcaption.appendChild(tags);
-                newimg.appendChild(tagcaption);
-                document.getElementById('bilder').appendChild(newimg);
-    
-                const photocaption = document.createElement("figcaption");
-                const photographer = document.createTextNode(element.user);
-                const photoby = document.createTextNode("Photo by: ");
-                photocaption.appendChild(photoby);
-                photocaption.appendChild(photographer);
-                newimg.appendChild(photocaption)
-                document.getElementById('bilder').appendChild(newimg);
-            });
-        }
-    }
+    imageLoop(json);
+        
 }
 //#endregion
